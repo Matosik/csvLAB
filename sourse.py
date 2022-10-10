34544aa68c479aa7976448fb7642a083a9013f51
Matosik <boga4iov.matvei2016@yandex.ru>
2022-10-10 21:18:35 +0300

    3 пункт
    Выполнен пункт "3"
    Был написан скрипт, создающий копию датасета таким образом, чтобы каждый файл из сходного датасета получил случайный номер от 0 до 10000. Так же скрипт создает новый csv файл для того чтобы можно было запоминать класс объекта

--- sourse.py
+++ sourse.py
@@ -11,8 +11,6 @@ def make_csv(name_csv):
         csv_file = csv.writer(file ,delimiter=';')
         csv_file.writerow(["Absolute path","Relative path","Class"])
         return csv_file
-    
-
 
 def make_file_abstract(name_class,full_way, file_name):#1 пункт
     
@@ -40,19 +38,50 @@ def porting(  name_abstract):# 2 пункт
             TO=f"dataset/{a[-2]}_{a[-1]}"
             shutil.copyfile(FROM,TO)
 
-
-
-
-
+        
+def random_name(file_abstract, new_abstract):# 3 путкт 
+    b=[]
+    for i in range(0,10001):
+        b.append(i)
+    c=random.sample(b,2200)
+    i=0
+    try:
+        os.mkdir("dataset_random")
+    except:
+        print("====ФАЙЛ ИМЕЕТСЯ====")
+    with open(f"{file_abstract}.csv",newline="") as file:
+        read= csv.DictReader(file,  delimiter=';')
+        for row in read:
+            FROM=row["Absolute path"]
+            a=FROM.split("/")
+            TO=f"dataset_random/{c[i]}.jpg"
+            i+=1
+            shutil.copyfile(FROM,TO)
+            class_obj=row["Class"]
+            with open(f"{new_abstract}.csv","a" ,newline="") as file_new:
+                csv_file = csv.writer(file_new ,delimiter=';') 
+                slash1="\ "
+                slash2="/"
+                way=os.getcwd()
+                counter=way.count(slash1[0])
+                full_way=f"{way.replace(slash1[0],slash2,counter)}/{TO}"
+                csv_file.writerow([full_way,TO,class_obj])
     
- 
-
 full_way="C:/Users/79093/Desktop/Application progra/parser/dataset/"
-
 class_dog="DogsIT" #папка где собаки 
 class_cat="CatIT"  #папка где кошки
+#========================= 1 пункт===========================
 file_abstract=input("Введите название файла-анотация для 1 пункта : ") 
 make_csv(file_abstract)
 make_file_abstract(class_dog, full_way,file_abstract)
 make_file_abstract(class_cat, full_way,file_abstract)
+
+#========================= 2 пункт===========================
 porting(file_abstract)
+
+#========================= 3 пункт===========================
+random_abstract=input("Введите название файла- анотация для 3 пункта: ")
+make_csv(random_abstract)
+random_name(file_abstract,random_abstract)
+
+#========================= 4 пункт===========================
